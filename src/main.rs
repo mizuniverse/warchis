072f7910752bb6f9461bd69972a2159628678779
MArlon Cepeda <marlons.cepeda@gmail.com>
2024-07-03 22:19:32 -0500

    agregado base en rust

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,69 @@
-use bevy::{
-    prelude::*,
-    window::{PresentMode},
-    //sprite::MaterialMesh2dBundle,
-};
-/*
-fn main() {
-    App::new()
-        .insert_resource(Msaa::default())
-        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
-        .add_plugins(DefaultPlugins.set(WindowPlugin{
-            primary_window: Some(Window{
-                title:"test game".into(),
-                resolution:(360.,640.).into(),
-                present_mode: PresentMode::AutoNoVsync,
-                fit_canvas_to_parent:true,
-                prevent_default_event_handling: false,
-                ..default()
-            }),
-            ..default()
-        }))
-            .add_systems(Startup, spawn_ball)
-        .run();
-}*/
+use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
+
+const BALL_SIZE: f32 = 5.;
+
+#[derive(Component)]
+struct Position(Vec2);
+
+#[derive(Component)]
+struct Ball;
+
+#[derive(Bundle)]
+struct BallBundle {
+    ball: Ball,
+    position: Position
+}
+
+impl BallBundle {
+    fn new() -> Self {
+        Self {
+            ball: Ball,
+            position: Position(Vec2::new(0., 10.)),
+        }
+    }
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_empty()
+            .insert(Camera2dBundle::default());
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_ball, spawn_camera))
+        .add_systems(Update, (project_positions))
         .run();
-}fn spawn_camera(mut commands: Commands) {
-    commands.spawn_empty()
-            .insert(Camera2dBundle::default());
 }
-fn spawn_ball(mut commands: Commands) {
-    println!("Spawning ball...");
-    commands.spawn_empty();
-}
-/*
 
-fn setup(
+fn spawn_ball(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn(Camera2dBundle::default());
+    println!("Spawning ball...");
 
-    // Circle
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
-        ..default()
-    });
+    let shape = Mesh::from(Circle::new(BALL_SIZE));
+    let color = ColorMaterial::from(Color::rgb(1., 0., 0.));
 
-    // Rectangle
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(50.0, 100.0)),
+    let mesh_handle = meshes.add(shape);
+    let material_handle = materials.add(color);
+
+    commands.spawn((
+        BallBundle::new(0., 0.),
+        MaterialMesh2dBundle {
+            mesh: mesh_handle.into(),
+            material: material_handle,
             ..default()
         },
-        transform: Transform::from_translation(Vec3::new(-50., 0., 0.)),
-        ..default()
-    });
-
-    // Quad
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes
-            .add(shape::Quad::new(Vec2::new(50., 100.)).into())
-            .into(),
-        material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
-        transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
-        ..default()
-    });
-
-    // Hexagon
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::RegularPolygon::new(50., 6).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::TURQUOISE)),
-        transform: Transform::from_translation(Vec3::new(150., 0., 0.)),
-        ..default()
-    });
+    ));
 }
-*/
 
+fn project_positions(
+    mut positionables: Query<(&mut Transform, &Position)>
+) {
+    for (mut transform, position) in &mut positionables {
+        transform.translation = position.0.extend(0.);
+    }
+}
