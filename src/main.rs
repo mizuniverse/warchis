48189d14a055406b48db81a7229b8868904d1373
MArlon Cepeda <marlons.cepeda@gmail.com>
2024-07-06 00:49:39 -0500

    [ADD][MOD] agregado sprite de pruebas, agregado codigo de prueba de movimiento

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,57 @@
 use bevy::prelude::*;
 
-
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins
+            .set(ImagePlugin::default_nearest())
+            .set(WindowPlugin{
+                primary_window: Some(Window{
+                    title:"titulo".into(),
+                    resolution:(640.0, 480.0).into(),
+                    resizable:true,
+                    ..default()
+                }),
+                ..default()
+            })
+            .build(),
+        )
+        .add_systems(Startup, setup)
+        .add_systems(Update,character_movement)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
+    commands.spawn(Camera2dBundle::default());
+
+    let texture = asset_server.load("character.png");
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite{
+            custom_size: Some(Vec2::new(100.0,100.0)),
+            ..default()
+        },
+        texture,
+        ..default()
+    });
+}
+
+fn character_movement(
+    mut characters: Query<(&mut Transform, &Sprite)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+){
+    for(mut transform, _) in &mut characters{
+        if input.pressed(KeyCode::W){
+            transform.translation.y += 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::S){
+            transform.translation.y -= 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::A){
+            transform.translation.x -= 100.0 * time.delta_seconds();
+        }
+        if input.pressed(KeyCode::D){
+            transform.translation.x += 100.0 * time.delta_seconds();
+        }
+    }
 }
