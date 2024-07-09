fb9acb5d63636fecf5613d27d1d2487318ba7644
MArlon Cepeda <marlons.cepeda@gmail.com>
2024-07-08 23:31:33 -0500

    [MOD] se agrega cerdito y ciclo de vida

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ pub struct Pig{
     pub lifetime: Timer,
 }
 
-#[derive(Component)]
+#[derive(Resource)]
 pub struct Money(pub f32);
 
 fn main() {
@@ -28,8 +28,9 @@ fn main() {
             })
             .build(),
         )
+        .insert_resource(Money(10.0))
         .add_systems(Startup, setup)
-        .add_systems(Update,character_movement)
+        .add_systems(Update,(character_movement,spawn_pig, pig_lifetime))
         .run();
 }
 
@@ -55,23 +56,74 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
 }
 
 fn character_movement(
-    mut characters: Query<(&mut Transform, &Sprite)>,
+    mut characters: Query<(&mut Transform, &Player)>,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ){
-    for(mut transform, _) in &mut characters{
+    for(mut transform, player) in &mut characters{
+        let movement_amount = player.speed * time.delta_seconds();
         if input.pressed(KeyCode::W){
-            transform.translation.y += 100.0 * time.delta_seconds();
+            transform.translation.y += movement_amount;
         }
         if input.pressed(KeyCode::S){
-            transform.translation.y -= 100.0 * time.delta_seconds();
+            transform.translation.y -= movement_amount;
         }
         if input.pressed(KeyCode::A){
-            transform.translation.x -= 100.0 * time.delta_seconds();
+            transform.translation.x -= movement_amount;
         }
         if input.pressed(KeyCode::D){
-            transform.translation.x += 100.0 * time.delta_seconds();
+            transform.translation.x += movement_amount;
         }
     }
 }
 
+fn spawn_pig(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    input: Res<Input<KeyCode>>,
+    mut money: ResMut<Money>,
+    player: Query<&Transform, With<Player>>,
+) {
+    if !input.just_pressed(KeyCode::Space) {
+        return;
+    }
+
+    let player_transform = player.single();
+
+    if money.0 >= 10.0 {
+        money.0 -= 10.0;
+        info!("Spent $10 on a pig, remaining money: ${:?}", money.0);
+
+        let texture = asset_server.load("pig.png");
+
+        commands.spawn((
+            SpriteBundle {
+                texture,
+                transform: *player_transform,
+                ..default()
+            },
+            Pig {
+                lifetime: Timer::from_seconds(2.0, TimerMode::Once),
+            },
+        ));
+    }
+}
+
+fn pig_lifetime(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut pigs: Query<(Entity, &mut Pig)>,
+    mut money: ResMut<Money>,
+) {
+    for (pig_entity, mut pig) in &mut pigs {
+        pig.lifetime.tick(time.delta());
+
+        if pig.lifetime.finished() {
+            money.0 += 15.0;
+
+            commands.entity(pig_entity).despawn();
+
+            info!("Pig sold for $15! Current Money: ${:?}", money.0);
+        }
+    }
+}
